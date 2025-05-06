527484b37d22f234022a7217f5cfbabd13f2f3c1
Hasbon <danhasoro@gmail.com>
2025-05-06 07:38:43 -0500

    Import files from new version

--- src/index.js
+++ src/index.js
@@ -8,29 +8,19 @@ import {
 } from 'react-router-dom'
 
 import './style.css'
-import Layout from './components/layout'
-import Home from './views/home'
-import Contact from './views/contact'
-import About from './views/about'
-import Portfolio from './views/portfolio'
-import Services from './views/services'
+import Inicio from './views/inicio'
+import Contactanos from './views/contactanos'
 import NotFound from './views/not-found'
 
 const App = () => {
   return (
     <Router>
-      <Layout>
-        <Switch>
-          <Route component={Home} exact path="/" />
-          <Route component={Contact} exact path="/contacto" />
-          <Route component={About} exact path="/nosotros" />
-          <Route component={Portfolio} exact path="/portafolio" />
-          <Route component={Portfolio} exact path="/portafolio/:id" />
-          <Route component={Services} exact path="/servicios" />
-          <Route component={NotFound} path="**" />
-          <Redirect to="**" />
-        </Switch>
-      </Layout>
+      <Switch>
+        <Route exact path="/" component={Inicio} />
+        <Route exact path="/contactanos" component={Contactanos} />
+        <Route path="/404" component={NotFound} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   )
 }
